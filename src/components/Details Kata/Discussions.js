d5ebc262186253adc68cda6e507d276a69d8bea8
Andrea0o0 <andreagarciagonzalez22@gmail.com>
2023-10-16 03:47:34 +0200

    discussions delete done

--- src/components/Details Kata/Discussions.js
+++ src/components/Details Kata/Discussions.js
@@ -7,7 +7,7 @@ import toast from 'react-hot-toast';
 import { AuthContext } from "../../context/AuthContext";
 
 export default function Discussions(){
-    const { isLoggedIn } = useContext(AuthContext); 
+    const { isLoggedIn, user } = useContext(AuthContext); 
 
     const {kata} = useOutletContext();
     const [discussions,setDiscussions] = useState(undefined)
@@ -32,7 +32,6 @@ export default function Discussions(){
         try {
           const response = await commentService.commentUser(kata._id)
           setUserDiscussion(response)
-          console.log(response)
           setLoading(false);
           setError(false);
         } catch (error) {
@@ -44,7 +43,8 @@ export default function Discussions(){
         const handleDeleteDiscussion = async () => {
             try {
                 await commentService.deleteComment(userdiscussion._id);
-                console.log('hi')
+                console.log('in')
+                window.location.reload(true)
                 setLoading(false);
                 setError(false);
             } catch (error) {
@@ -56,7 +56,8 @@ export default function Discussions(){
       useEffect(() => {
         getDiscussions()
         handleOwnDiscussions()
-        console.log(userdiscussion)
+        // window.location.reload(true)
+        // console.log(userdiscussion)
         // eslint-disable-next-line
       }, [])
 
@@ -66,6 +67,7 @@ export default function Discussions(){
             const response = await commentService.createComment({kata:kata._id,comment:newDiscussion});
             setNewDiscussion('')
             setDiscussions(response)
+            window.location.reload(true)
             setLoading(false);
             setError(false);
               toast.success("Comment added",{style:{backgroundColor:'#1a1e24', color:'white'}})
@@ -84,25 +86,19 @@ export default function Discussions(){
             {loading && <div className='flex justify-center mt-20'><img width='10%' src={Loading} alt='loading'/></div>}
             {!loading && discussions && 
             <div className="flex flex-col items-center">
-            {userdiscussion !== undefined ? 
+            {userdiscussion !== null||undefined ? 
                 <>
                     <div className='kataComment flex flex-col items-center text-white ml-2 my-6'>
                         <p className='text-sm my-2'>My comment</p>
-                        <div className="kataDiscussions border-1 rounded-lg">
-                            <div className="m-4 flex flex-wrap justify-center">
-                                <div className='w-11/12 flex flex-wrap justify-center'>
+                        <div className='kataDiscussions border-1 rounded-lg m-4 p-4 flex flex-wrap justify-center w-11/12 flex flex-wrap justify-center'>
                                     <div className="divKataDiscussions flex justify-center items-center text-white w-3/5 mb-2">
                                         <img className=' rounded-full' width='10%' src={userdiscussion.user.image} alt={`Image_${userdiscussion.user.username}`}/>
                                         <h3 className="ml-4 text-sm p-1 px-10 rounded-full">{`@${userdiscussion.user.username}`}</h3>
                                     </div>  
-                                <p className={`pt-1 ${textInput}`}>{userdiscussion.comment}</p>
-                                <>
-                                  <button className={`text-sm p-1.5 px-6 mt-2 mx-2 bg-background-lightcolor rounded-full border-1 border-background-lightcolor hover:border-white hover:text-white`} onClick={handleNewComment}>Edit</button>  
+                                <p className={`pt-1 m-1 ${textInput}`}>{userdiscussion.comment}</p>
+                                <>  
                                   <button className={`text-sm p-1.5 px-6 mt-2 bg-background-lightcolor rounded-full border-1 border-background-lightcolor hover:border-white hover:text-white`} onClick={handleDeleteDiscussion}>Delete</button>
-                                </>
-                                
-                                </div>
-                            </div>   
+                                </>  
                         </div>
                     </div>
                 </>:
@@ -116,20 +112,22 @@ export default function Discussions(){
                 }
                 {discussions.length > 0 ? 
                 <>
-                <div className="kataDiscussions border-1 rounded-lg">
-                    {discussions.sort((a,b)=>a.updatedAt - b.updatedAt).reverse().map((elem,i) => {
-                        return elem.user !== null && elem.user._id !== userdiscussion.user._id  &&
-                        <div key={i} className="m-4 flex flex-wrap justify-center">
-                            <div className='w-11/12 flex flex-wrap justify-center'>
-                                <div className="divKataDiscussions flex justify-center items-center text-white w-3/5 mb-2">
-                                    <img className=' rounded-full' width='10%' src={elem.user.image} alt={`Image_${elem.user.username}`}/>
-                                    <h3 className="ml-4 text-sm p-1 px-10 rounded-full">{`@${elem.user.username}`}</h3>
-                                </div>  
-                            <p className={`pt-1 ${textInput}`}>{elem.comment}</p>
+                <p className='text-sm mb-2 pb-2 text-white '>Comments</p>
+                    {discussions.sort((a,b) => a.updatedAt - b.updatedAt).reverse().map((elem,i) => {
+                        return elem.user._id !== user._id  &&
+                        <div key={i} className="kataDiscussions border-1 rounded-lg">
+                            <div className="m-4 flex flex-wrap justify-center">
+                                <div className='w-11/12 flex flex-wrap justify-center'>
+                                    <div className="divKataDiscussions flex justify-center items-center text-white w-3/5 mb-2">
+                                        <img className=' rounded-full' width='10%' src={elem.user.image} alt={`Image_${elem.user.username}`}/>
+                                        <h3 className="ml-4 text-sm p-1 px-10 rounded-full">{`@${elem.user.username}`}</h3>
+                                    </div>  
+                                <p className={`pt-1 ${textInput}`}>{elem.comment}</p>
+                                </div>
+                            
                             </div>
-                           
-                        </div>})}
-                </div>                
+                        </div> })}
+                               
                 </>:
                 <>
                     <div className="flex justify-center my-4">
